1ceca29a48c0e6bad9df7f31cf0c81ee42c44abf
Lukasz <lukmar993@gmail.com>
2015-06-14 00:53:01 +0200

    init commit

--- app/src/main/java/pl/cukierbergonrush/MainActivity.java
+++ app/src/main/java/pl/cukierbergonrush/MainActivity.java
@@ -2,6 +2,7 @@ package pl.cukierbergonrush;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
@@ -10,37 +11,44 @@ import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     LoginButton loginButton;
     CallbackManager callbackManager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FacebookSdk.sdkInitialize(getApplicationContext());
+        FacebookSdk.sdkInitialize(this.getApplicationContext());
+
         callbackManager = CallbackManager.Factory.create();
-        loginButton = (LoginButton) findViewById(R.id.login_button);
-        loginButton.setReadPermissions("user_friends");
 
-        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
-            @Override
-            public void onSuccess(LoginResult loginResult) {
-            }
+        LoginManager.getInstance().registerCallback(callbackManager,
+                new FacebookCallback<LoginResult>() {
+                    @Override
+                    public void onSuccess(LoginResult loginResult) {
+                        // App code
+                        Log.d(TAG,"onSuccess");
+                    }
 
-            @Override
-            public void onCancel() {
-                // App code
-            }
+                    @Override
+                    public void onCancel() {
+                        // App code
+                        Log.d(TAG,"onError");
 
-            @Override
-            public void onError(FacebookException exception) {
-                // App code
-            }
-        });
+                    }
+
+                    @Override
+                    public void onError(FacebookException exception) {
+                        // App code
+                        Log.d(TAG,"onError");
+                    }
+                });
     }
 
 
